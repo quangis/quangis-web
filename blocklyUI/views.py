7e935e885ad7da3afe886947c93abea223c34927
Niels Steenbergen <n.steenbergen@uu.nl>
2022-07-31 13:12:11 +0200

    Send query to MarkLogic & fix HTTP DIGEST auth.

--- blocklyUI/views.py
+++ blocklyUI/views.py
@@ -10,9 +10,35 @@ from django.http import HttpResponse
 from django.http import HttpResponseBadRequest, JsonResponse
 
 import json
+import urllib  # cf <https://realpython.com/urllib-request/>
+from rdflib import Graph
+from rdflib.plugins.stores.sparqlstore import SPARQLStore
+from cct.language import question2query
 
 from questionParser.models import QuestionParser
 from questionParser.models import TypesToQueryConverter
+
+# While you can pass authorization to the `SPARQLStore` object, this
+# authorization is basic; DIGEST authorization is only handled for
+# `SPARQLUpdateStore`. To work around this, we add a custom opener to the
+# `urllib.request` module that SPARQLConnector is using under the hood. It
+# seems we could also pull the external dependency of SPARQLWrapper, as
+# mentioned in <https://github.com/RDFLib/rdflib/issues/343>, but RDFLib
+# dropped this dependency in <https://github.com/RDFLib/rdflib/pull/744>, so
+# I'd rather not re-import it (but see also
+# <https://github.com/RDFLib/sparqlwrapper/pull/126>). Note also that there was
+# a QuAnGIS-MarkLogic client at <https://github.com/quangis/marklogic_client>,
+# but I'd rather not roll my own for this sort of thing (plus: we do have to
+# inferface with `rdflib`).
+passmgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+passmgr.add_password(
+    None, "https://qanda.soliscom.uu.nl:8000/v1/graphs/sparql",
+    "username", "password")
+auth_handler = urllib.request.HTTPDigestAuthHandler(passmgr)
+opener = urllib.request.build_opener(auth_handler)
+urllib.request.install_opener(opener)
+store = SPARQLStore("https://qanda.soliscom.uu.nl:8000/v1/graphs/sparql")
+graph = Graph(store)
 
 # [SC] for testing
 # from django.apps import apps
@@ -29,7 +55,9 @@ def parseQuestion(qStr):
     # [SC] the query json object is inside qParsed variable
     # [SC] you can send the marklogic query from here
     # [SC] also attach the JSON-LD workflow to qParsed
-    
+
+    qParsed['matches'] = [str(wf) for wf in question2query(qParsed).run(graph)]
+
     return qParsed
     
 
