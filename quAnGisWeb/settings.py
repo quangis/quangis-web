9381ca9da724380f4531c2b907fc9add297f8642
Niels Steenbergen <n.steenbergen@uu.nl>
2022-08-17 12:08:08 +0200

    Fix wrong URL here.
    Not sure what happened.

--- quAnGisWeb/settings.py
+++ quAnGisWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_KEY']
 
-TDB_URL = "https://qanda.soliscom.uu.nl:8000/v1/graphs/sparql"
+TDB_URL = "https://qanda.soliscom.uu.nl:8000"
 TDB_USER = os.environ['TDB_USER']
 TDB_PASS = os.environ['TDB_PASS']
 
